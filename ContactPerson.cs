d8524f510d0bde00f5ee3a77e669b2695154b0a4
Vallem Balu <vbnreddy5@gmail.com>
2021-07-20 17:58:54 +0530

    ContactPerson Details

---
+++ ContactPerson.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class ContactPerson
+	{ 
+		public String FirstName;
+		public String LastName;
+		public String Address;
+		public String City;
+		public String State;
+		public String Zip;
+		public String PhoneNumber;
+		public String Email;
+
+        public ContactPerson()
+        {
+        }
+
+        public ContactPerson(String FirstName, String LastName, String Address, String City, String State, String PhoneNumber, String Zip, String Email)
+		{
+			this.FirstName = FirstName;
+			this.LastName = LastName;
+			this.Address = Address;
+			this.PhoneNumber = PhoneNumber;
+			this.State = State;
+			this.Zip = Zip;
+			this.City = City;
+			this.Email = Email;
+		}
+		public void print()
+		{
+			Console.WriteLine(FirstName + " \t  " + LastName + " \t  " + Address + " \t  " + City +" \t" + State + " \t  " + PhoneNumber + " \t " + Zip + " \t" +Email); 
+		}
+	}
+}
